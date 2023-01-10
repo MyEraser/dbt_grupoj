9523921c0751b10935f5fc90427d5881e942c7d7
EsdrasNewPost <esdras.cruz@newpost.com>
2023-01-10 16:20:16 -0300

    criando uma view de testes para estrurar a tabela listings

---
+++ grupoj/models/redshift/listings.sql
@@ -0,0 +1,26 @@
+create view dbt_airbnb.listings as (
+	select 
+		id,
+		_file as arquivo_origem,
+		_line as linha,
+		"_modified" as data_modificacao,
+		name as nome,
+		host_id,
+		host_name,
+		neighbourhood as bairro,
+		latitude,
+		longitude,
+		room_type as tipo_sala,
+		price as preco ,
+		minimum_nights as minimo_noites,
+		number_of_reviews as numero_revisoes,
+		last_review as ultima_revisao,
+		reviews_per_month as revisoes_mes,
+		calculated_host_listings_count as contagem_calculada_listagens,
+		availability_365,
+		number_of_reviews_ltm,
+		"_fivetran_synced"	
+	FROM s3.covid_airbnd
+	where _file like '%listings.csv%'
+
+)
